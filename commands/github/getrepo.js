bc19b5fb07bd209725ccf381dc896e4f9022c086
HiMyNameIsSunny <thaigrrs@gmail.com>
2020-03-26 02:09:54 +0200

    +Documentation and Error messaging

--- commands/github/getrepo.js
+++ commands/github/getrepo.js
@@ -2,6 +2,8 @@ const commando = require('discord.js-commando');
 const fetch = require("node-fetch");
 const { RichEmbed } = require('discord.js');
 
+// Add return statement for repo not found.
+
 class getRepository extends commando.Command {
 
     constructor(client){
@@ -9,7 +11,7 @@ class getRepository extends commando.Command {
             name: 'repo',
             group: 'github',
             memberName: 'repo',
-            description: 'Retreives repository and its information',
+            description: 'Retreives repository and its information, !repo <userName>/<repoName>',
             args: [
                 {
                     key: 'repos',
@@ -29,17 +31,27 @@ class getRepository extends commando.Command {
         }
     }
 
+    /*
+    Retreives the repository.
+    Convert response to JSON object and passes it to create the Message.
+    Finally sends the message to the discord server.
+    */
     getRepo(message, repository){
         fetch("https://api.github.com/repos/" + repository)
         .then(response => response.json())
         .then(responseData => {
-            //message.channel.send("The repo name is " + responseData.full_name + " The owner is " + responseData.owner.login);
-            var eMessage = this.createMessage(responseData)
-            message.channel.send(eMessage);
+            if(responseData.hasOwnProperty('message')){
+                message.channel.send("The repository " + repository + " was not found.");
+            } else {
+                var eMessage = this.createMessage(responseData)
+                message.channel.send(eMessage);
+            }
+            
         })
         .catch(err => console.log(err))        
     }
 
+    // Creates embedded message with details from the fetch response.
     createMessage(response){
         const embeddedMessage = new RichEmbed()
         .setColor(0xFF0092)
@@ -56,6 +68,7 @@ class getRepository extends commando.Command {
         return embeddedMessage;
     }
 
+    // Converts a date string from YYYY-mm-dd to dd-mm-YYYY 
     convertDate(date){
         var time = date.substring(11,19);
         var result = date.substring(0,10);
